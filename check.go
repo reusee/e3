c4a88ce70241052837a0362b369ff888a8735259
reus <reusee@gmail.com>
2020-11-23 10:04:46 +0800

    refactor Check, Catch

--- check.go
+++ check.go
@@ -10,43 +10,43 @@ func (t *thrownError) String() string { // NOCOVER
 }
 
 func Check(err error, fns ...WrapFunc) {
-	if err != nil {
-		for _, fn := range fns {
-			err = fn(err)
-		}
-		if _, ok := err.(*Stacktrace); !ok {
-			err = NewStacktrace()(err)
-		}
-		panic(&thrownError{
-			err: err,
-		})
+	if err == nil {
+		return
 	}
+	for _, fn := range fns {
+		err = fn(err)
+	}
+	if _, ok := err.(*Stacktrace); !ok {
+		err = NewStacktrace()(err)
+	}
+	panic(&thrownError{
+		err: err,
+	})
 }
 
 func Catch(errp *error, fns ...WrapFunc) {
 	if errp == nil {
 		return
 	}
+	var err error
 	if p := recover(); p != nil {
 		if e, ok := p.(*thrownError); ok {
-			err := e.err
-			for _, fn := range fns {
-				err = fn(err)
-			}
-			*errp = err
+			err = e.err
 		} else {
 			panic(p)
 		}
 	} else {
-		err := *errp
-		if err == nil {
-			return
+		if *errp != nil {
+			err = *errp
 		}
-		for _, fn := range fns {
-			err = fn(err)
-		}
-		*errp = err
 	}
+	if err == nil {
+		return
+	}
+	for _, fn := range fns {
+		err = fn(err)
+	}
+	*errp = err
 }
 
 var C = Check
