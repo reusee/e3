3c30b753ebe0b32fef98718798a805d235bb1831
reus <reusee@gmail.com>
2020-12-04 14:34:29 +0800

    mark testing.T.Helpers

--- testing.go
+++ testing.go
@@ -3,12 +3,15 @@ package e3
 import "testing"
 
 func TestingFatalf(t *testing.T, format string, args ...any) WrapFunc {
+	t.Helper()
 	return func(err error) Error {
+		t.Helper()
 		t.Fatal(err)
 		return NewInfo(format, args...)(err)
 	}
 }
 
 func TestingFatal(t *testing.T) WrapFunc {
+	t.Helper()
 	return TestingFatalf(t, "testing Fatal")
 }
